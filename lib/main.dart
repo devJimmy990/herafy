396549401a795905b1ea0a645d458568070dd4fd
Meroothman <01119923095a@gmail.com>
2024-07-07 16:29:59 +0300

    marge

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,9 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:herafy/core/helper/routes.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:herafy/core/helper/firebase_options.dart';
-<<<<<<< HEAD
 import 'package:herafy/presentation/admin/admin_home/admin_home.dart';
-=======
 import 'package:herafy/core/helper/shared_preferences.dart';
 import 'package:herafy/domain/cubit/auth/auth_cubit.dart';
->>>>>>> dad19b8c8d08809f8715abd8c9b38683f318c8ec
 import 'package:herafy/presentation/auth/login/login.dart';
 import 'package:herafy/presentation/auth/signup/register.dart';
 import 'package:herafy/presentation/auth/signup/register_fill.dart';
@@ -36,16 +33,6 @@ class MyApp extends StatelessWidget {
           primarySwatch: Colors.blue,
         ),
         locale: const Locale("ar"),
-        routes: {
-          Routes.login: (context) => const LoginPage(),
-          Routes.register: (context) => const RegisterationPage(),
-          Routes.registerFill: (context) => const RegisterFillData(),
-        },
-        initialRoute: Routes.login,
-        // home: const LoginPage(),
-      ),
-<<<<<<< HEAD
-      locale: const Locale("ar"),
       routes: {
         Routes.login: (context) => const LoginPage(),
         Routes.register: (context) => const RegisterationPage(),
@@ -53,9 +40,7 @@ class MyApp extends StatelessWidget {
         Routes.adminHome: (context) => const AdminHome(),
       },
       initialRoute: Routes.login,
-      
-=======
->>>>>>> dad19b8c8d08809f8715abd8c9b38683f318c8ec
+      ),     
     );
   }
 }
