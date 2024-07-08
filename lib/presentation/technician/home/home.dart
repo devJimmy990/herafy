4d32f532a442e9194b61267848c8d429d6f84ac4
Muhammed Gamal <devjimmy99@gmail.com>
2024-07-08 03:34:41 +0300

    profile

--- lib/presentation/technician/home/home.dart
+++ lib/presentation/technician/home/home.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:herafy/core/widgets/custom_app_bar.dart';
+import 'package:herafy/domain/cubit/order/order_cubit.dart';
 
 class TechnicianHomePage extends StatelessWidget {
   const TechnicianHomePage({super.key});
@@ -10,7 +12,14 @@ class TechnicianHomePage extends StatelessWidget {
       appBar: MyCustomAppBar(
         title: "Technician",
       ),
-      body: Center(child: Text("Technician Home Page")),
+      body: Column(
+        children: [
+          Center(child: Text("Technician Home Page")),
+          // FutureBuilder(future: future, builder: (builder, snapshot) {
+          //   return Text(context.read<OrderCubit>().);
+          // })
+        ],
+      ),
     );
   }
 }
